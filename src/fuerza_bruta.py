35b90940ac5d9b76f2d5c2421789c9f82dbdc863
Sebastian Rendon <srendon521@gmail.com>
2026-05-16 13:09:04 -0500

    Codigo de fuerza bruta funcionando correctamente.

--- src/fuerza_bruta.py
+++ src/fuerza_bruta.py
@@ -1,2 +1,56 @@
 def resolver(paquetes, camiones):
-    return "Fuerza bruta pendiente"
+
+    mejor = []
+    max_paquetes = 0
+
+    def fuerza_bruta(indice, asignacion, cargas):
+
+        nonlocal mejor
+        nonlocal max_paquetes
+
+        if indice == len(paquetes):
+
+            cantidad = sum(len(c) for c in asignacion)
+
+            if cantidad > max_paquetes:
+                max_paquetes = cantidad
+                mejor = [c.copy() for c in asignacion]
+
+            return
+
+        paquete = paquetes[indice]
+        id_paquete = paquete[0]
+        peso = paquete[1]
+
+        for i in range(len(camiones)):
+
+            capacidad = camiones[i][1]
+
+            if cargas[i] + peso <= capacidad:
+
+                cargas[i] += peso
+                asignacion[i].append(id_paquete)
+
+                fuerza_bruta(indice + 1, asignacion, cargas)
+
+                cargas[i] -= peso
+                asignacion[i].pop()
+
+        fuerza_bruta(indice + 1, asignacion, cargas)
+
+    asignacion_inicial = [[] for _ in camiones]
+    cargas_iniciales = [0] * len(camiones)
+
+    fuerza_bruta(0, asignacion_inicial, cargas_iniciales)
+
+    # Formatear igual que el otro
+    asignacion_final = {}
+
+    for i in range(len(camiones)):
+        id_camion = camiones[i][0]
+        asignacion_final[id_camion] = mejor[i] if i < len(mejor) else []
+
+    return {
+        "total_entregados": max_paquetes,
+        "asignacion": asignacion_final
+    }
